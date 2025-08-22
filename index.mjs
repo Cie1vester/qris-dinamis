6b608b67ae7b062e0cba52191c13677403685f1d
Sylvester <sylcatt@gmail.com>
2025-08-22 16:06:34 +0800

    Added exports for ESModule

--- index.mjs
+++ index.mjs
@@ -1,2 +1,4 @@
-export { makeFile } from './src/makeFile';
-export { makeString } from './src/makeString';
+export { makeQr } from './src/makeQr.js';
+export { makeFile } from './src/makeFile.js';
+export { makeString } from './src/makeString.js';
+export { dataQris, toCRC16, pad } from './lib/index.js';
